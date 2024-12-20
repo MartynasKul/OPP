cbeb008607d587ba173e72e72c5867d9d8a7053f
Bonks <58624357+Veedas@users.noreply.github.com>
2024-12-20 03:34:15 +0200

    Fix for proxy

--- killthemall/server/src/com/javakaian/shooter/shapes/WeaponSecurityProxy.java
+++ killthemall/server/src/com/javakaian/shooter/shapes/WeaponSecurityProxy.java
@@ -2,11 +2,11 @@ package com.javakaian.shooter.shapes;
 
 public class WeaponSecurityProxy extends BaseWeapon {
 
-    private final BaseWeapon realWeapon; // The actual weapon
-    private final Player player;        // Player associated with the weapon
+    private final BaseWeapon realWeapon;
+    private final Player player;
 
     public WeaponSecurityProxy(BaseWeapon realWeapon, Player player) {
-        super(realWeapon.bullet); // Pass the bullet to the proxy
+        super(realWeapon.bullet);
         this.realWeapon = realWeapon;
         System.out.println("WeaponProxy.");
         this.player = player;
@@ -15,23 +15,18 @@ public class WeaponSecurityProxy extends BaseWeapon {
     @Override
     public void bulletShot(){
         System.out.println("Bullet shot by "+ player.getName());
+        realWeapon.bulletShot();
     }
 
     @Override
     public IBullet getBullet(float x, float y, float size, float angle, int id) {
-        // Security rules before allowing the weapon to shoot
+
         if (!player.isAlive()) {
             System.out.println("Security Proxy: Player is dead. Cannot shoot.");
-            return null; // Prevent shooting if player is dead
+            return null;
         }
 
-        if (player.getScore() > 300) {
-            //player.setName("tomas")
-            System.out.println("Security Proxy: Pasiekta 300.");
-            return null; // Prevent shooting if score is too low
-        }
 
-        // Forward the call to the real weapon if all checks pass
         return realWeapon.getBullet(x, y, size, angle, id);
     }
 }
